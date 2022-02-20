1c0226aeff9c1e75d39e369afa1e13f6633fd9c7
Ikko-T <skyblueboy32@gmail.com>
2022-02-20 11:46:32 +0900

    Set up API to fronend

--- script.js
+++ script.js
@@ -40,3 +40,85 @@ const displayUsers = async () => {
 };
 
 displayUsers();
+
+// =========================================================
+
+const button = document.getElementById('user-button');
+const firstNameElement = document.getElementById('user-first-name');
+const lastNameElement = document.getElementById('user-last-name');
+const ageElement = document.getElementById('user-age');
+const companyElement = document.getElementById('user-company');
+const emailElement = document.getElementById('user-email');
+const phoneNumberElement = document.getElementById('user-phone-number');
+const twitterElement = document.getElementById('user-twitter');
+const messageElement = document.getElementById('user-message');
+
+function genders() {
+  let genderElements = document.getElementsByName('gender');
+  let len = genderElements.length;
+  let checkValue = '';
+
+  for (let i = 0; i < len; i++) {
+    if (genderElements[i].checked) {
+      checkValue = genderElements[i].value;
+    }
+  }
+
+  if (checkValue === '') {
+    checkValue = genderElements[0].value;
+  }
+  return checkValue;
+}
+const genderElement = Number(genders());
+
+const registerUser = async () => {
+  const url = `${baseUrl}/api/v1/users`;
+  const userParams = {
+    user: {
+      first_name: firstNameElement.value,
+      last_name: lastNameElement.value,
+      age: ageElement.value,
+      company: companyElement.value,
+      email: emailElement.value,
+      phone_number: phoneNumberElement.value,
+      twitter: twitterElement.value,
+      message: messageElement.value,
+      gender: genderElement,
+    },
+  };
+
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userParams),
+  });
+
+  if (!response.ok) {
+    throw new Error(`${response.status} (${response.statusText})`);
+  }
+
+  const user = await response.json();
+  return user;
+};
+
+const userForm = async () => {
+  try {
+    const user = await registerUser();
+    addUser(user);
+    firstNameElement.value = '';
+    lastNameElement.value = '';
+    ageElement.value = '';
+    companyElement.value = '';
+    emailElement.value = '';
+    phoneNumberElement.value = '';
+    twitterElement.value = '';
+    messageElement.value = '';
+    genderElement.value = '';
+  } catch (e) {
+    alert(e);
+  }
+};
+
+button.addEventListener('click', userForm);
